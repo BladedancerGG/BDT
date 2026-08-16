3bd9944987a4ae4c1d6d7e1577c108201798ab01
Bladedancer <bladedancer@bladedancer.net>
2026-08-16 18:47:57 +1100

    refactor: changed awkward translation
    changes:
    - changed the browsePlugs translation to be a static one as some sockets could have awkward names

--- src/components/tooltip/PlugTooltip.tsx
+++ src/components/tooltip/PlugTooltip.tsx
@@ -104,7 +104,7 @@ export function PlugTooltip({
           <DestinySymbol name="mouseLeft" className="plug-tooltip__action-key" />
           {equippable
             ? t("equipPerk")
-            : t("browsePlugs", { type: browseLabel ?? "" })}
+            : t("browsePlugs")}
         </p>
       )}
     </div>
